b28daa50a9b539dd03a7fd1a590ca19819532194
queq1890 <yuji.matsumoto@sun-asterisk.com>
2022-05-10 13:33:07 +0900

    Update sample application

---
+++ app/src/components/MessageList.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+
+type Props = {
+  messageList: string[];
+};
+
+const MessageList: FC<Props> = (props) => {
+  return (
+    <Stack>
+      {props.messageList.map((message) => {
+        return <Box>{message}</Box>;
+      })}
+    </Stack>
+  );
+};
+
+export default MessageList;
