3055ca3fea173d8bdcecfe7be77cfabf7a4cef5e
Yuji Matsumoto <yuji.matsumoto@sun-asterisk.com>
2022-05-10 13:34:50 +0900

    Feature/chapter3 (#9)
    * Write lesson until the markup section
    
    * Props の箇所
    
    * Update sample application

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MessageList from './components/MessageList';
+
+// TODO: ユーザーの入力によってmessageList が可変になるようになる
+const messageList = ['こんにちは', 'テスト', 'ですです'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MessageList messageList={messageList} />
     </div>
   );
 }
